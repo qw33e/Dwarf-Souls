79001f6868479bb031f535033178005c0f29323f
Qweee <106638341+qw33e@users.noreply.github.com>
2022-07-18 12:08:05 -0400

    Delete DwarfSouls directory

--- DwarfSouls/compile.bat
+++
@@ -1,2 +0,0 @@
-REM Automatically generated from Makefile
-..\..\..\bin\lcc -o DwarfSouls.gb mini_souls.c map_map.c map_tiles.c
--- DwarfSouls/map_tiles.h
+++
@@ -1,7 +0,0 @@
-#ifndef __map_tiles_h_INCLUDE
-#define __map_tiles_h_INCLUDE
-
-#define map_tilesBank 0
-extern const unsigned char map_tiles[];
-
-#endif
